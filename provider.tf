9140a74040f08e713e8a6094a7bb698143c18fda
lfeldman <lukasz.feldman@oracle.com>
2022-06-15 12:00:21 +0200

    Fixed provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,6 @@
 ## Copyright (c) 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "= 4.79"
-    }
-  }
-}
-
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
